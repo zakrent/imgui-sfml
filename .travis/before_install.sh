a358269ad0f408556f0e4158d88bd6fcbe758b5a
Kjetil Andresen <kjetil.andrese@gmail.com>
2017-12-10 09:08:14 +0100

    Added OSX build

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,17 +2,23 @@
 
 # TODO: GCC 7
 
-# For C++17 we need newer CMake
-mkdir /tmp/cmake
-wget https://cmake.org/files/v3.10/cmake-3.10.0.tar.gz -P /tmp/cmake/
-cd /tmp/cmake
-tar xzf cmake-3.10.0.tar.gz
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew update
+; fi
 
-# Newer version of GCC
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-
-# SFML 2.3 (instead of 2.1 in Trusty)
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu xenial universe"
-sudo apt-get update -qq
-
-cd $TRAVIS_BUILD_DIR
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  # For C++17 we need newer CMake
+  mkdir /tmp/cmake
+  wget https://cmake.org/files/v3.10/cmake-3.10.0.tar.gz -P /tmp/cmake/
+  cd /tmp/cmake
+  tar xzf cmake-3.10.0.tar.gz
+  
+  # Newer version of GCC
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  
+  # SFML 2.3 (instead of 2.1 in Trusty)
+  sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu xenial universe"
+  sudo apt-get update -qq
+  
+  cd $TRAVIS_BUILD_DIR
+; fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# Install CMake 3.10
-cd /tmp/cmake/cmake-3.10.0/
-./configure --prefix=/opt/cmake
-make
-sudo make install
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew install sfml
+; fi
 
-# Install SFML 2.3
-sudo apt-get install -y libsfml-dev
-
-# Install g++-6 and set as default version
-sudo apt-get install -qq g++-6
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
-
-cd $TRAVIS_BUILD_DIR
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  # Install CMake 3.10
+  cd /tmp/cmake/cmake-3.10.0/
+  ./configure --prefix=/opt/cmake
+  make
+  sudo make install
+  
+  # Install SFML 2.3
+  sudo apt-get install -y libsfml-dev
+  
+  # Install g++-6 and set as default version
+  sudo apt-get install -qq g++-6
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
+  
+  cd $TRAVIS_BUILD_DIR
+; fi
