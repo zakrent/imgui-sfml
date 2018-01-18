c774b412d4cdaf13559a2504ac0052193b289527
Kjetil Andresen <kjetil.andrese@gmail.com>
2018-01-18 19:20:51 +0100

    travis: remove need for newer g++ and cmake

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,18 +5,8 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 fi
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  # Install CMake 3.10
-  cd /tmp/cmake/cmake-3.10.0/
-  ./configure --prefix=/opt/cmake
-  make
-  sudo make install
-  
   # Install SFML 2.3
   sudo apt-get install -y libsfml-dev
-  
-  # Install g++-6 and set as default version
-  sudo apt-get install -qq g++-6
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
   
   cd $TRAVIS_BUILD_DIR
 fi
